6586c3d57c7127de05cfe6ea8f560b1f503d0fc3
cd <jc-blazquez@hotmail.com>
2020-01-25 11:01:12 +0100

    Cambios en el properties configuracion

--- springboot-servicio-item/src/main/java/com/formacion/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacion/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.formacion.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +25,15 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RequestMapping
 public class ItemController {
 
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
 	protected static final String ITEM_CONTROLLER = "/item";
 	protected static final String LISTAR_ITEM = "/listar";
 	protected static final String VER_ITEM = "/ver";
+	protected static final String CONFIG = "/obtener-config";
+
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@Autowired
 	@Qualifier("serviceRest")
@@ -29,6 +42,17 @@ public class ItemController {
 	@GetMapping(LISTAR_ITEM)
 	public List<Item> listar() {
 		return itemService.findAll();
+	}
+
+	@GetMapping(CONFIG)
+	public ResponseEntity<?> config(@Value("${server.port}") String puerto) {
+		log.info(texto);
+
+		final Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 	@HystrixCommand(fallbackMethod = "metodoAlternativo")
